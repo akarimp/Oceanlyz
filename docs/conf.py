2e85a94d3f8a8ec15f5e8b62a0a0feb67ee2cf5f
Arash Karimpour <52866480+akarimp@users.noreply.github.com>
2019-07-14 21:47:55 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'ARASH KARIMPOUR'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.4.5'
+release = '1.4'
 
 
 # -- General configuration ---------------------------------------------------
